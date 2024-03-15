3ec1251903eb1c37ec38ad03dc295f8c837c2459
Gregory Arcentales <gregoarcenta@gmail.com>
2024-03-15 01:29:02 -0500

    cambio de ruta para verificacion y registro

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,37 +1,36 @@
-import { createTransport } from 'nodemailer';
+import { createTransport } from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 export const sendEmailVerification = (email: string, token: string) => {
+  return new Promise((resolve, reject) => {
+    const smtOptions: SMTPTransport.Options = {
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.USER_EMAIL,
+        accessToken: process.env.USER_PASS,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN
+      }
+    };
 
-    return new Promise((resolve, reject) => {
+    const transporter = createTransport(smtOptions);
 
-        const smtOptions: SMTPTransport.Options ={
-            service: 'gmail',
-            auth: {
-                type: 'OAuth2',
-                user: process.env.USER_EMAIL,
-                accessToken: process.env.USER_PASS,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                refreshToken: process.env.REFRESH_TOKEN
-            }
-        };
+    const mailOptions: Mail.Options = {
+      from: email,
+      to: email,
+      subject: "Devtalles Sorteos - Verificación de correo electrónico.",
+      html: `<p>Haz clic en el siguiente <a href="${process.env.DOMAIN}/admin/registro/${token}">enlace</a> para verificar tu correo electrónico y completar tu registro</p>`
+    };
 
-        const transporter = createTransport(smtOptions);
-
-        const mailOptions = {
-            from: email,
-            to: email,
-            subject: 'Devtalles Sorteos - Verificación de correo electrónico.',
-            text: `Haz clic en el siguiente enlace para verificar tu correo electrónico: ${process.env.DOMAIN}/verificar?token=${token}`
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(info);
-            }
-        });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(info);
+      }
     });
-}
+  });
+};
